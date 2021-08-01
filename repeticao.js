c65c5bfb9a7d0578ebcf59073251a77d08b07119
jvitormcs <vitormouracs@gmail.com>
2021-08-01 13:41:01 -0300

    Versão Inicial

--- repeticao.js
+++ repeticao.js
@@ -1,5 +1,15 @@
 var numSorteado = 10;
 
-for (var i = 0; i < 100; i++){
-    
+//var tabuada = 7;
+
+//for (var i = 0; i < 10; i++){
+//    console.log("Valor de " + tabuada + " x " + i + " = " + (tabuada * i))
+//}
+
+
+for (var i = 0; i< 100; i++){
+    if (numSorteado === i){
+        console.log ('Seu numero foi encontrado');
+        break;
+    }
 }
