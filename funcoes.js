5565461d2c2c9d3c33edc6211ec6d5c2bf978791
jvitormcs <vitormouracs@gmail.com>
2021-08-04 22:04:37 -0300

    Aula funcoes

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+function soma (operadorA, operadorB) {
+
+    let resultadoC = operadorA + operadorB;
+    return resultadoC;
+}
+
+
+function olaBruce (nome) {
+    console.log("Olá eu sou o Bruce! você é o " + nome)
+}
+
+
+var resultadoSoma = soma(1,2);
+var resultadoNovoSoma = soma(10,66);
+
+
+
+console.log(resultadoSoma)
+console.log(resultadoNovoSoma)
+
+
+olaBruce("João Vitor")
+
+console.log(olaBruce)
+
