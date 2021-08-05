e83057c248ef359352a37f567b2292aa51b3ad92
jvitormcs <vitormouracs@gmail.com>
2021-08-04 21:05:42 -0300

    Aula com while

--- repeticao.js
+++ repeticao.js
@@ -1,4 +1,4 @@
-var numSorteado = 10;
+// var numSorteado = 10;
 
 //var tabuada = 7;
 
@@ -7,9 +7,27 @@ var numSorteado = 10;
 //}
 
 
-for (var i = 0; i< 100; i++){
-    if (numSorteado === i){
-        console.log ('Seu numero foi encontrado');
-        break;
+//for (var i = 0; i< 100; i++){
+//    if (numSorteado === i){
+//        console.log ('Seu numero foi encontrado');
+//        break;
+//    }
+//}
+
+
+var achou = false;
+
+var numSorteado = 10;
+var possivelValor = 0;
+
+while (!achou) {
+    possivelValor += 1;
+    if (numSorteado === possivelValor) {
+        achou = true;
+        console.log ("Numero encontrado: " + numSorteado)
+    } else {
+        console.log (
+            "Possivel Valor não corresponde ao numero sorteado " + possivelValor
+        );
     }
 }
