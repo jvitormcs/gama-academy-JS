10f48b57fc9045eddb86deccbaa67e28ee50e678
jvitormcs <vitormouracs@gmail.com>
2021-08-07 17:29:47 -0300

    Validador finalizado

--- validadorCPF/script/script.js
+++ validadorCPF/script/script.js
@@ -1,38 +1,55 @@
-console.log("JavaScript carregado...")
+console.log('Javascript carregado');
 
-
-function validaCPF(cpf){
-    console.log(cpf.length)
+function validaCPF(cpf) {
     if (cpf.length != 11) {
-       return false;
+        return false;
     } else {
-
-        let numeros = cpf.substring(0, 9); 
-        let digitos = cpf.substring(9); 
-
-        let soma = 0;
-        for (var i = 10; i > 1; i--) {
-         
-        soma += numeros.charAt(10 - i) * i;
-        }
         
-        console.log("soma é " + soma);
+        var numeros = cpf.substring(0, 9);
+        var digitos = cpf.substring(9);
 
-        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+        var soma = 0;
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+
+        var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        // Validação do primeiro dígito do CPF
+        if (resultado != digitos.charAt(0)) {
+            return false;
+        }
+
+        soma = 0;
+        numeros = cpf.substring(0, 10);
+
+        for (var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+
+        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        // Validação do segundo dígito do CPF
+        if (resultado != digitos.charAt(1)) {
+            return false;
+        }
 
         return true;
     }
 }
 
 function validacao() {
-    console.log("Iniciando validação CPF")
-    let cpf = document.getElementById("cpf_digitado").value;
+    console.log('Iniciando validação CPF');
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
     
-    let resultadoValidacao = validaCPF(cpf);
+    var cpf = document.getElementById('cpf_digitado').value;
+    
+    var resultadoValidacao = validaCPF(cpf);
 
     if (resultadoValidacao) {
-        document.getElementById("success").style.display = "block";
+        document.getElementById('success').style.display = 'block';
     } else {
-        document.getElementById("error").style.display = "block";
+        document.getElementById('error').style.display = 'block';
     }
 }
